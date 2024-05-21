a4717874c59f9aceb5444c5deb2d3d30dbbfb864
Palloma Gualter <pallomakcgualter@gmail.com>
2024-05-21 12:44:12 -0300

    feat: adicionando as entidades Médico e Endereço

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        medicoRepository.save(new Medico(dados));
     }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -20,7 +20,7 @@ public class Medico {
     private String nome;
     private String email;
 
-    //private String telefone;
+    private String telefone;
 
     private String crm;
 
@@ -33,7 +33,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
-        //this.telefone = dados.telefone();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
